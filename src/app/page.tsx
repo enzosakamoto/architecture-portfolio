ca0efc6b718baf6889069110844da157574e9b47
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-07-04 17:29:10 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-'use client'
-import { useState } from 'react'
 import Navbar from './components/navbar/page'
 
 export default function Home() {
-  const [count, setCount] = useState<number>(0)
-
   return (
     <>
       <Navbar />
-      <div className="flex h-screen flex-col items-center justify-center gap-5">
-        <h1 className="text-2xl">Clique no botão</h1>
-        <button
-          className="w-20 rounded-lg border-2 border-black p-2 text-xl"
-          onClick={() => setCount(count + 1)}
-        >
-          {count}
-        </button>
-      </div>
+      <div className="flex h-screen flex-col items-center justify-center gap-5"></div>
     </>
   )
 }
