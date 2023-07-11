791651352010c54a8592efcae737315a1a31a5a1
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-07-11 20:33:12 -0300

    added fade effect on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,35 +8,93 @@ import {
   AiOutlineGithub
 } from 'react-icons/ai'
 import { MdEmail } from 'react-icons/md'
+import { useEffect, useState } from 'react'
+
+interface Form {
+  name: string
+  email: string
+  message: string
+}
 
 export default function Contact() {
   const { english } = useLanguage()
+  const [forms, setForms] = useState<Form>({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const [fade, setFade] = useState<boolean>(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setFade(true)
+    }, 200)
+  }, [])
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    alert(
+      `Nome: ${forms.name} \nE-mail: ${forms.email} \nMensagem: ${forms.message}`
+    )
+    setForms({
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
   return (
     <main className="flex flex-col overflow-x-hidden bg-gradient-to-b from-white via-blue-100 to-blue-200 py-20">
       <Navbar english={english} />
-      <section className="flex w-full flex-col items-center justify-center gap-28 py-32  ">
+      <section className="flex w-full transform flex-col items-center justify-center gap-28 py-32 transition-all duration-500">
         <p className="text-5xl font-bold">{english ? 'Contact' : 'Contato'}</p>
-        <div className="flex w-full flex-col gap-8 px-10 lg:flex-row">
-          <form className="flex w-full flex-col items-center gap-8 rounded-2xl bg-blue-50 p-10 drop-shadow-md lg:w-2/3">
+        <div
+          className={`flex w-full transform flex-col gap-8 px-10 transition-all duration-500 lg:flex-row ${
+            fade ? 'translate-x-0 opacity-100' : 'translate-x-10 opacity-0'
+          }`}
+        >
+          <form
+            onSubmit={handleSubmit}
+            className="flex w-full flex-col items-center gap-8 rounded-2xl bg-blue-50 p-10 drop-shadow-md lg:w-2/3"
+          >
             <div className="flex w-full flex-col gap-2 text-xl">
-              <label htmlFor="">{english ? 'Name' : 'Nome'}</label>
+              <label htmlFor="" className="font-medium">
+                {english ? 'Name' : 'Nome'}
+              </label>
               <input
                 type="text"
+                value={forms.name}
+                onChange={(e) => setForms({ ...forms, name: e.target.value })}
                 className="transform border-b-2 border-blue-400 bg-transparent outline-none transition-all duration-500 focus:border-blue-800"
               />
             </div>
             <div className="flex w-full flex-col gap-2 text-xl">
-              <label htmlFor="">E-mail</label>
+              <label htmlFor="" className="font-medium">
+                E-mail
+              </label>
               <input
                 type="text"
+                value={forms.email}
+                onChange={(e) => setForms({ ...forms, email: e.target.value })}
                 className="transform border-b-2 border-blue-400 bg-transparent outline-none transition-all duration-500 focus:border-blue-800"
               />
             </div>
             <div className="flex w-full flex-col gap-2 text-xl">
-              <label htmlFor="">{english ? 'Message' : 'Mensagem'}</label>
-              <textarea className="h-20 transform resize-none border-b-2 border-blue-400 bg-transparent outline-none transition-all duration-500 focus:border-blue-800" />
+              <label htmlFor="" className="font-medium">
+                {english ? 'Message' : 'Mensagem'}
+              </label>
+              <textarea
+                value={forms.message}
+                onChange={(e) =>
+                  setForms({ ...forms, message: e.target.value })
+                }
+                className="h-20 transform resize-none border-b-2 border-blue-400 bg-transparent outline-none transition-all duration-500 focus:border-blue-800"
+              />
             </div>
-            <button className="transform place-content-center rounded-xl bg-blue-200 px-2 py-2 text-lg font-medium drop-shadow-md transition-all duration-500 hover:scale-105 hover:bg-blue-500 sm:w-1/6 sm:px-0">
+            <button
+              onClick={() => handleSubmit}
+              disabled={
+                forms.name === '' || forms.email === '' || forms.message === ''
+              }
+              className="transform place-content-center rounded-xl bg-blue-200 px-2 py-2 text-lg font-medium drop-shadow-md transition-all duration-500 hover:scale-105 hover:bg-blue-500 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500 disabled:hover:scale-100 sm:w-1/6 sm:px-0"
+            >
               Enviar
             </button>
           </form>
