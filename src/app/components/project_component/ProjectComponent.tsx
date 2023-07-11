909bb841ed577e6be823152e2bd7e4c815680363
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-07-11 13:42:59 -0300

    fixed gap in project component

--- src/app/components/project_component/ProjectComponent.tsx
+++ src/app/components/project_component/ProjectComponent.tsx
@@ -39,7 +39,7 @@ export default function ProjectComponent({
           alt={title}
           className="max-h-xs ml-4 mr-4 w-56 sm:mr-0 sm:max-w-xs"
         />
-        <div className="mx-8 flex h-full flex-col justify-around text-sm sm:text-lg">
+        <div className="mx-8 flex h-full flex-col justify-around gap-4 text-sm sm:gap-2 sm:text-lg xl:gap-0">
           <p className="text-2xl font-bold">{title}</p>
           <p>{text}</p>
           <p className="font-bold">{formatTechs(techs)}</p>
