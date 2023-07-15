3f572c3f2e467a007dd852097b3fc4579f111fb2
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-07-15 09:27:39 -0300

    fixed resend api and several things

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,18 +1,7 @@
 import { NextResponse } from 'next/server'
-// import { Resend } from 'resend'
-
-// const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: Request) {
   const { name, email, message } = await request.json()
-  // resend.emails.send({
-  //   from: 'onboarding@resend.dev',
-  //   to: 'enzo.sak@hotmail.com',
-  //   subject: 'Nova mensagem de formulário',
-  //   html: `<p>Nome: ${name}</p><p>Email: ${email}</p><p>Mensagem: ${message}</p>`
-  // })
-
-  // return NextResponse.json({ message: 'Email sent' })
 
   const res = await fetch('https://api.resend.com/emails', {
     method: 'POST',
